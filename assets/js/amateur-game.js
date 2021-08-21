4c16b8f77d7bc651a16f3b1e02d0d219b24568e7
malikdobbs <malik-dobbs@hotmail.com>
2021-08-21 14:26:18 +0000

    javascript function added that when user clicks card score increments

--- assets/js/amateur-game.js
+++ assets/js/amateur-game.js
@@ -1,4 +1,4 @@
-var totalTime = 100;
+var totalTime = 100; // Countdown timer helped to be created by James McDowell from stackoverflow.com credited in readme.MD
 var amateurGameCountDown = setInterval(function(){
     if (totalTime <= 0) {
         clearInterval(amateurGameCountDown);
@@ -9,26 +9,57 @@ var amateurGameCountDown = setInterval(function(){
     totalTime -= 1;
 }, 1000)
 
-var audio = new Audio("assets/audio/crowd-applause.wav");
+var audio = new Audio("assets/audio/crowd-applause.wav"); // New Game audio
 audio.loop= true;
 
-function playMusic() { //start game audio
+function playMusic() { 
     if(!audio.paused) {  
         audio.pause();  
         audio.currentTime = 0;  
     }
     else {
-        audio.play();  /* To make it play again */
+        audio.play();  // To make it play again
     }
 };
 
+class NbaWorld {
+    constructor(cards) {
+        this.cardsArray = cards;
+        
+        this.scoreCard = document.getElementById("score");
+    }
+    playGame() {
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.matchedCards = [];
+        this.busy = true;
+    }
+    flipCard(card) {
+        if(this.canFlipCard(card)) {
+            this.totalCLicks++;
+            this.scoreCard.innerText = this.totalClicks++;
+        }
+    }
+    canFlipCard(card) {
+        return true;
+        //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
+}
+
 function ready() {
     let textOverlays = Array.from(document.getElementsByClassName("text-overlay")); // creates an array for all overlay text
-    let gameCards = Array.from(document.getElementsByClassName("game-card")); // creates array for all cards in the game
+    let cards = Array.from(document.getElementsByClassName("game-card"));
+    let game = new NbaWorld(100, cards);
 
     textOverlays.forEach(overlay => { // iterate through overlay array
         overlay.addEventListener("click", () => {
             overlay.classList.remove('visible')
+            game.playGame();
+        });
+    });
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            game.flipCard(card);
         })
     })
 }
